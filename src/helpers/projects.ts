9dcea901aaf05ee34487f7f70b8529e1262fe781
beelzick <kacperzki123@wp.pl>
2021-11-09 23:00:16 +0100

    add project

---
+++ src/helpers/projects.ts
@@ -0,0 +1,39 @@
+const projects = [
+    {
+        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634578204/eze-miniature-2.webp',
+        title: 'EzeSneakers',
+        description: 'EzeSneakers is an e-commerce showcase website that offers restored shoes from brands like Nike, Adidas, Puma. This project is built on top of React and Next.js.',
+        gitHref: 'https://github.com/beelzick/eze-sneakers',
+        demoHref: 'https://eze-sneakers.vercel.app/'
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634687297/portfolio-thumb.webp',
+        title: 'Portfolio',
+        description: 'This portfolio is built on top of React.',
+        gitHref: 'https://github.com/beelzick/portfolio-react',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1636043436/github-repo-finder_ql5qhs.webp',
+        title: ' GitHub Repo Finder',
+        description: 'Project made with GitHub GraphQL API and React it allows to search for repository by name.',
+        gitHref: 'https://github.com/beelzick/github-repo-finder',
+        demoHref: 'https://github-repo-finder-sepia.vercel.app/'
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1636493255/todo-thumb_wbjydw.webp',
+        title: 'Todo Maker',
+        description: 'Simple todo app made with React and Apollo.',
+        gitHref: '',
+        demoHref: ''
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634595524/workout-places-thumb_eokaxv.webp',
+        title: 'Workout Places',
+        description: 'This showcase project mainly focuses on the backend side with express. It includes CRUD operations and database relationships.',
+        gitHref: 'https://github.com/beelzick/workout-places',
+        demoHref: 'https://workout-places-showcase.herokuapp.com/'
+    },
+
+]
+
+export default projects
--- src/sections/SectionProjects.tsx
+++ src/sections/SectionProjects.tsx
@@ -3,37 +3,8 @@ import ProjectBox from '../components/ProjectBox/ProjectBox'
 import styles from './Section.module.css'
 import { v4 as uuidv4 } from 'uuid';
 import ProjectsContainer from '../components/ProjectBox/ProjectsContainer'
+import projects from '~/helpers/projects';
 
-const projects = [
-    {
-        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634578204/eze-miniature-2.webp',
-        title: 'EzeSneakers',
-        description: 'EzeSneakers is an e-commerce showcase website that offers restored shoes from brands like Nike, Adidas, Puma. This project is built on top of React and Next.js.',
-        gitHref: 'https://github.com/beelzick/eze-sneakers',
-        demoHref: 'https://eze-sneakers.vercel.app/'
-    },
-    {
-        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634687297/portfolio-thumb.webp',
-        title: 'Portfolio',
-        description: 'This portfolio is built on top of React.',
-        gitHref: 'https://github.com/beelzick/portfolio-react',
-    },
-    {
-        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1636043436/github-repo-finder_ql5qhs.webp',
-        title: ' GitHub Repo Finder',
-        description: 'Project made with GitHub GraphQL API it allows to search for repository by name.',
-        gitHref: 'https://github.com/beelzick/github-repo-finder',
-        demoHref: 'https://github-repo-finder-sepia.vercel.app/'
-    },
-    {
-        imgUrl: 'https://res.cloudinary.com/dfvpybkta/image/upload/v1634595524/workout-places-thumb_eokaxv.webp',
-        title: 'Workout Places',
-        description: 'This showcase project mainly focuses on the backend side with express. It includes CRUD operations and database relationships.',
-        gitHref: 'https://github.com/beelzick/workout-places',
-        demoHref: 'https://workout-places-showcase.herokuapp.com/'
-    },
-
-]
 
 export default function SectionProjects() {
     const typographyRest = {
@@ -44,7 +15,7 @@ export default function SectionProjects() {
     return (
         <section className={styles.sectionProjects} id='projects'>
             <TypographySlide  {...typographyRest}>
-                SHOWCASE PROJECTS
+                PROJECTS
             </TypographySlide>
             <ProjectsContainer>
                 {projects.map(({ imgUrl, title, description, gitHref, demoHref }, i) => (
